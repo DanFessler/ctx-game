745a58856fbc11bfeaf8738e60c6a92022030813
Dan Fessler <fezzinate@gmail.com>
2025-09-25 00:55:36 -0700

    adding new default and test scenes

--- src/game/index.ts
+++ src/game/index.ts
@@ -1,13 +1,14 @@
 import Game from "../engine/Game";
 const behaviors = import.meta.glob("./behaviors/*.ts", { eager: true });
-import scene from "./scenes/default.json";
+import defaultScene from "./scenes/default.json";
+import testScene from "./scenes/TestScene.json";
 // import native from "../editor/callNative";
 
 let game: Game;
 if (!Game.instance) {
   game = new Game(960, 540, 64, 1);
   game.registerBehaviors(behaviors);
-  game.loadScene(scene);
+  game.loadScene(defaultScene);
   game.start();
   // game.play();
 } else {
